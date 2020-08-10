d1bb7d98c73fd64407fc8c8641467ae1af0eac3d
qodhrkawk <gabriel1120@naver.com>
2020-08-10 23:34:21 +0900

    프로젝트 생성

---
+++ Melon_CloneCoding/Melon_CloneCoding/Source/VCs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Melon_CloneCoding
+//
+//  Created by Yunjae Kim on 2020/08/10.
+//  Copyright © 2020 Yunjae Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Melon_CloneCoding/Pods/Target Support Files/Pods-Melon_CloneCoding/Pods-Melon_CloneCoding-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Melon_CloneCoding : NSObject
+@end
+@implementation PodsDummy_Pods_Melon_CloneCoding
+@end
